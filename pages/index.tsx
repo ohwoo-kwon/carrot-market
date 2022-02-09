d6943d4327acae58a27c782b464bf4405deb21fb
ohwoo-kwon <kwonow1@gmail.com>
2022-02-09 14:10:11 +0900

    4.8 More Modifiers

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,24 +2,19 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <form className="flex flex-col space-y-2  p-5 ">
+    <div className="flex flex-col space-y-2  p-5">
       <input
-        type="text"
-        required
-        placeholder="Username"
-        className="border p-1 peer border-gray-400 rounded-md "
+        type="file"
+        className="file:border-0 file:rounded-xl file:px-5 file:text-white file:bg-purple-400
+        file:transition-colors file:cursor-pointer file:hover:bg-white file:hover:text-purple-400"
       />
-      <span className="hidden peer-invalid:block peer-invalid:text-red-500">
-        This input is invalid
-      </span>
-      <span className="hidden peer-valid:block peer-valid:text-teal-500">
-        Awesome username
-      </span>
-      <span className="hidden peer-hover:block peer-hover:text-amber-500">
-        Hello
-      </span>
-      <input type="submit" value="Login" className="bg-white" />
-    </form>
+      <p className="first-letter:text-7xl first-letter:hover:font-bold first-line:text-indigo-500">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae,
+        deleniti. Eaque dolorum earum aperiam vitae perferendis, iusto
+        recusandae labore id saepe ratione neque officiis, maxime iure, facilis
+        possimus consequuntur excepturi!
+      </p>
+    </div>
   );
 };
 
