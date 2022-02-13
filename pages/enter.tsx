e93ecf0615ee35e74f7e2e5e8fbb671e5a2e18df
ohwoo-kwon <kwonow1@gmail.com>
2022-02-13 10:25:14 +0900

    5.5 Upload Item

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -41,13 +41,14 @@ const Enter: NextPage = () => {
           </div>
         </div>
         <form className="flex flex-col mt-8">
-          <label className="text-sm font-medium text-gray-700">
+          <label htmlFor="input" className="text-sm font-medium text-gray-700">
             {method === "email" ? "Email address" : null}
             {method === "phone" ? "Phone number" : null}
           </label>
           <div className="mt-1">
             {method === "email" ? (
               <input
+                id="input"
                 type="email"
                 className="appearance-none w-full px-3
                 py-2 border border-gray-300 rounded-md
@@ -62,6 +63,7 @@ const Enter: NextPage = () => {
                   +82
                 </span>
                 <input
+                  id="input"
                   type="number"
                   className="appearance-none w-full px-3
                   py-2 border border-gray-300 rounded-r-md
