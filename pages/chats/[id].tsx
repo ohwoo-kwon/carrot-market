c7690d177b7e101d4522c14713b0e444a46d1c28
ohwoo <kwonow1@gmail.com>
2022-02-15 16:43:34 +0900

    5.17 Layout part Two

--- pages/chats/[id].tsx
+++ pages/chats/[id].tsx
@@ -1,40 +1,43 @@
 import type { NextPage } from "next";
+import Layout from "../../components/layout";
 
 const ChatDetail: NextPage = () => {
   return (
-    <div className="py-10 pb-16 px-10 space-y-4">
-      <div className="flex items-start space-x-2">
-        <div className="h-8 w-8 rounded-full bg-slate-400" />
-        <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
-          <p>Hi how much are you selling them for?</p>
+    <Layout canGoBack>
+      <div className="py-10 pb-16 px-10 space-y-4">
+        <div className="flex items-start space-x-2">
+          <div className="h-8 w-8 rounded-full bg-slate-400" />
+          <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+            <p>Hi how much are you selling them for?</p>
+          </div>
         </div>
-      </div>
-      <div className="flex flex-row-reverse items-start space-x-2 space-x-reverse">
-        <div className="h-8 w-8 rounded-full bg-slate-400" />
-        <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
-          <p>I want ￦20,000</p>
+        <div className="flex flex-row-reverse items-start space-x-2 space-x-reverse">
+          <div className="h-8 w-8 rounded-full bg-slate-400" />
+          <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+            <p>I want ￦20,000</p>
+          </div>
         </div>
-      </div>
-      <div className="flex items-start space-x-2">
-        <div className="h-8 w-8 rounded-full bg-slate-400" />
-        <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
-          <p>미쳤어?</p>
+        <div className="flex items-start space-x-2">
+          <div className="h-8 w-8 rounded-full bg-slate-400" />
+          <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+            <p>미쳤어?</p>
+          </div>
         </div>
-      </div>
-      <div className="bg-white fixed py-2 bottom-0 inset-x-0">
-        <div className="flex relative max-w-md  w-full mx-auto items-center">
-          <input
-            type="text"
-            className="shadow-sm pr-12 rounded-full w-full border-gray-300 focus:ring-orange-400 focus:outline-none focus:border-orange-400"
-          />
-          <div className="absolute inset-y-0 flex py-1.5 pr-1.5 right-0">
-            <button className="flex items-center bg-orange-400 rounded-full px-3 text-sm text-white hover:bg-orange-600 focus:ring-2 focus:ring-offset-2 focus:ring-orange-400 focus:outline-none">
-              &rarr;
-            </button>
+        <div className="bg-white fixed py-2 bottom-0 inset-x-0">
+          <div className="flex relative max-w-md  w-full mx-auto items-center">
+            <input
+              type="text"
+              className="shadow-sm pr-12 rounded-full w-full border-gray-300 focus:ring-orange-400 focus:outline-none focus:border-orange-400"
+            />
+            <div className="absolute inset-y-0 flex py-1.5 pr-1.5 right-0">
+              <button className="flex items-center bg-orange-400 rounded-full px-3 text-sm text-white hover:bg-orange-600 focus:ring-2 focus:ring-offset-2 focus:ring-orange-400 focus:outline-none">
+                &rarr;
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Layout>
   );
 };
 
