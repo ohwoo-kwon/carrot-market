3917167a956c85dc7ad9cef2ee98bff06b3755ab
ohwoo <kwonow1@gmail.com>
2022-02-17 15:02:28 +0900

    6.7 API Routes

--- libs/client.ts
+++ libs/client.ts
@@ -1,10 +1,3 @@
 import { PrismaClient } from "@prisma/client";
 
-const client = new PrismaClient();
-
-client.user.create({
-  data: {
-    email: "hi@naver.com",
-    name: "hi",
-  },
-});
+export default new PrismaClient();
