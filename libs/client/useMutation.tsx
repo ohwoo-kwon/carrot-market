9e5ceafa08b6cfff66e3cfc98cd47ab6a47fc0c2
ohwoo <kwonow1@gmail.com>
2022-02-18 13:03:59 +0900

    8.3 Clean Code part Two

--- libs/client/useMutation.tsx
+++ libs/client/useMutation.tsx
@@ -1,14 +1,31 @@
 import { useState } from "react";
 
-export default function useMutation(
-  url: string
-): [
-  (data?: any) => void,
-  { loading: boolean; data: undefined | any; error: undefined | any }
-] {
-  const [loading, setLoading] = useState(false);
-  const [data, setData] = useState<undefined | any>(undefined);
-  const [error, setError] = useState<undefined | any>(undefined);
-  function mutation(data: any) {}
-  return [mutation, { loading, data, error }];
+interface UseMutationState {
+  loading: boolean;
+  data?: object;
+  error?: object;
+}
+type UseMutationResult = [(data: any) => void, UseMutationState];
+
+export default function useMutation(url: string): UseMutationResult {
+  const [state, setState] = useState({
+    loading: false,
+    data: undefined,
+    error: undefined,
+  });
+  function mutation(data: any) {
+    setState({ ...state, loading: true });
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json().catch(() => {}))
+      .then((json) => setState({ ...state, data: json }))
+      .catch((error) => setState({ ...state, error }))
+      .finally(() => setState({ ...state, loading: false }));
+  }
+  return [mutation, state];
 }
