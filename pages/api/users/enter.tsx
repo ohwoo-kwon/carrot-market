78553dbc0136794bbf28563cf51c452393277f3a
ohwoo <kwonow1@gmail.com>
2022-02-18 21:20:47 +0900

    9.1 Accounts Logic

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,9 +1,53 @@
+import { prisma } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
 import client from "../../../libs/server/client";
 import withHandler from "../../../libs/server/withHandler";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
+  const { phone, email } = req.body;
+  const user = await client.user.upsert({
+    where: {
+      ...(phone ? { phone: +phone } : {}),
+      ...(email ? { email } : {}),
+    },
+    create: {
+      name: "Anonymous",
+      ...(phone ? { phone: +phone } : {}),
+      ...(email ? { email } : {}),
+    },
+    update: {},
+  });
+  console.log(user);
+  // if (email) {
+  //   user = await client.user.findFirst({
+  //     where: {
+  //       email,
+  //     },
+  //   });
+  //   if (!user) {
+  //     user = await client.user.create({
+  //       data: {
+  //         name: "Anonymous",
+  //         email,
+  //       },
+  //     });
+  //   }
+  // }
+  // if (phone) {
+  //   user = await client.user.findFirst({
+  //     where: {
+  //       phone: +phone,
+  //     },
+  //   });
+  //   if (!user) {
+  //     user = await client.user.create({
+  //       data: {
+  //         name: "Anonymous",
+  //         phone: +phone,
+  //       },
+  //     });
+  //   }
+  // }
   return res.status(200).end();
 }
 
