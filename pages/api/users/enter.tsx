14793a749efb9ed3b47dc5228220bf2d1f262a72
ohwoo <kwonow1@gmail.com>
2022-02-19 10:02:55 +0900

    9.4 Sending SMS

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,6 +1,9 @@
+import twilio from "twilio";
 import { NextApiRequest, NextApiResponse } from "next";
 import client from "../../../libs/server/client";
 import withHandler, { ResponseType } from "../../../libs/server/withHandler";
+
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
 async function handler(
   req: NextApiRequest,
@@ -26,6 +29,13 @@ async function handler(
       },
     },
   });
+  if (phone) {
+    await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_SID_M,
+      to: process.env.TWILIO_PHONE!,
+      body: `Your login token is ${payload}`,
+    });
+  }
   return res.json({
     ok: true,
   });
