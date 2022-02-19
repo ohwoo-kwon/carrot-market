f7986d22ea4780acb35cb75e45387817a3b96a22
ohwoo <kwonow1@gmail.com>
2022-02-19 19:44:56 +0900

    11.4 Product Detail

---
+++ pages/api/products/[id].ts
@@ -0,0 +1,33 @@
+import { withAPISession } from "@libs/server/withSession";
+import { NextApiRequest, NextApiResponse } from "next";
+import client from "@libs/server/client";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const { id } = req.query;
+  const product = await client.product.findUnique({
+    where: {
+      id: +id.toString(),
+    },
+    include: {
+      user: {
+        select: {
+          id: true,
+          name: true,
+          avatar: true,
+        },
+      },
+    },
+  });
+  res.json({ ok: true, product });
+}
+
+export default withAPISession(
+  withHandler({
+    methods: ["GET"],
+    handler,
+  })
+);
