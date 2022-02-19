68f4e507976163c3dfe63db42a5a25d30e648b35
ohwoo <kwonow1@gmail.com>
2022-02-19 10:19:00 +0900

    9.5 Sending Email

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,7 +1,10 @@
+import mail from "@sendgrid/mail";
 import twilio from "twilio";
 import { NextApiRequest, NextApiResponse } from "next";
 import client from "../../../libs/server/client";
 import withHandler, { ResponseType } from "../../../libs/server/withHandler";
+
+mail.setApiKey(process.env.API_KEY);
 
 const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
@@ -30,10 +33,18 @@ async function handler(
     },
   });
   if (phone) {
-    await twilioClient.messages.create({
+    const message = await twilioClient.messages.create({
       messagingServiceSid: process.env.TWILIO_SID_M,
       to: process.env.TWILIO_PHONE!,
       body: `Your login token is ${payload}`,
+    });
+  } else if (email) {
+    const email = await mail.send({
+      from: "kwonow1@naver.com",
+      to: "kwonow1@gmail.com",
+      subject: "Your Carrot Market Verification Email",
+      text: `Your token is ${payload}`,
+      html: `<strong>Your token is ${payload}</strong>`,
     });
   }
   return res.json({
