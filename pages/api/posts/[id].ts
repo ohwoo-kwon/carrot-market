a50a2a0d1c9cfff01439b5b347e3cb931ad3c587
ohwoo <kwonow1@gmail.com>
2022-02-20 15:23:40 +0900

    12.2 Post Detail

---
+++ pages/api/posts/[id].ts
@@ -0,0 +1,54 @@
+import { withAPISession } from "@libs/server/withSession";
+import { NextApiRequest, NextApiResponse } from "next";
+import client from "@libs/server/client";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    query: { id },
+  } = req;
+  const post = await client.post.findUnique({
+    where: {
+      id: +id.toString(),
+    },
+    include: {
+      user: {
+        select: {
+          id: true,
+          name: true,
+          avatar: true,
+        },
+      },
+      answers: {
+        select: {
+          answer: true,
+          id: true,
+          user: {
+            select: {
+              id: true,
+              name: true,
+              avatar: true,
+            },
+          },
+        },
+      },
+      _count: {
+        select: {
+          answers: true,
+          wonders: true,
+        },
+      },
+    },
+  });
+  res.json({ ok: true, post });
+}
+
+export default withAPISession(
+  withHandler({
+    methods: ["GET"],
+    handler,
+  })
+);
