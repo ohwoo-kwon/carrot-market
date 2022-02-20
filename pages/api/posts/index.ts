bd6f706eb2396901d6b7fb6c5790beaf6e2d0294
ohwoo <kwonow1@gmail.com>
2022-02-20 13:40:40 +0900

    12.1 Forms and Handlers

---
+++ pages/api/posts/index.ts
@@ -0,0 +1,32 @@
+import { withAPISession } from "@libs/server/withSession";
+import { NextApiRequest, NextApiResponse } from "next";
+import client from "@libs/server/client";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    body: { question },
+    session: { user },
+  } = req;
+  const post = await client.post.create({
+    data: {
+      question,
+      user: {
+        connect: {
+          id: user?.id,
+        },
+      },
+    },
+  });
+  res.json({ ok: true, post });
+}
+
+export default withAPISession(
+  withHandler({
+    methods: ["POST"],
+    handler,
+  })
+);
