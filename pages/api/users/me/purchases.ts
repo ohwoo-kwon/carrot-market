0655588390a71826a63433a75ceb31bb6031f006
ohwoo-kwon <kwonow1@gmail.com>
2022-02-21 17:54:49 +0900

    13.2 Handlers

---
+++ pages/api/users/me/purchases.ts
@@ -0,0 +1,29 @@
+import { withAPISession } from "@libs/server/withSession";
+import { NextApiRequest, NextApiResponse } from "next";
+import client from "@libs/server/client";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    session: { user },
+  } = req;
+  const purchases = await client.purchase.findMany({
+    where: {
+      userId: user?.id,
+    },
+    include: {
+      product: true,
+    },
+  });
+  res.json({ ok: true, purchases });
+}
+
+export default withAPISession(
+  withHandler({
+    methods: ["GET"],
+    handler,
+  })
+);
