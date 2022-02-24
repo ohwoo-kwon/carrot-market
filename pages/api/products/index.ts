82570b63f156c5bd4db088ade7a695d1d5451e93
ohwoo-kwon <kwonow1@gmail.com>
2022-02-24 22:52:38 +0900

    15.8 Product Images

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -20,14 +20,14 @@ async function handler(
     res.json({ ok: true, products });
   }
   if (req.method === "POST") {
-    const { name, price, description } = req.body;
+    const { name, price, description, photoId } = req.body;
     const { user } = req.session;
     const product = await client.product.create({
       data: {
         name,
         price: +price,
         description,
-        image: "",
+        image: photoId,
         user: {
           connect: {
             id: user?.id,
