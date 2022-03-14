dc2e17265a0bd7fdf966e200e905e9f5e1d32853
ohwoo-kwon <kwonow1@gmail.com>
2022-03-14 09:47:35 +0900

    19.8 SSR + Authentication

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,10 +1,13 @@
-import type { NextPage } from "next";
+import type { NextPage, NextPageContext } from "next";
 import Link from "next/link";
 import Layout from "@components/layout";
 import useUser from "@libs/client/useUser";
-import useSWR from "swr";
+import useSWR, { SWRConfig } from "swr";
 import { Review, User } from "@prisma/client";
 import { cls } from "@libs/client/utils";
+import Image from "next/image";
+import { withSsrSession } from "@libs/server/withSession";
+import client from "@libs/server/client";
 
 interface ReviewWithUser extends Review {
   createdBy: User;
@@ -19,13 +22,16 @@ const Profile: NextPage = () => {
   const { user } = useUser();
   const { data } = useSWR<ReviewsResponse>("/api/reviews");
   return (
-    <Layout hasTabBar title="나의 캐럿">
+    <Layout hasTabBar title="나의 캐럿" seoTitle="Profile">
       <div className="px-4">
         <div className="flex items-center mt-4 space-x-3">
           {user?.avatar ? (
-            <img
+            <Image
               src={`https://imagedelivery.net/CWmSXRI4a4GtzEmC6jdPoA/${user?.avatar}/avatar`}
-              className="w-16 h-16 bg-slate-500 rounded-full"
+              alt="user-image"
+              width={64}
+              height={64}
+              objectFit="cover"
             />
           ) : (
             <div className="w-16 h-16 bg-slate-500 rounded-full" />
@@ -147,4 +153,30 @@ const Profile: NextPage = () => {
     </Layout>
   );
 };
-export default Profile;
+
+const Page: NextPage<{ profile: User }> = ({ profile }) => {
+  return (
+    <SWRConfig
+      value={{
+        fallback: {
+          "/api/users/me": { ok: true, profile },
+        },
+      }}
+    >
+      <Profile />
+    </SWRConfig>
+  );
+};
+
+export const getServerSideProps = withSsrSession(async function ({
+  req,
+}: NextPageContext) {
+  const profile = await client.user.findUnique({
+    where: { id: req?.session.user?.id },
+  });
+  return {
+    props: { profile: JSON.parse(JSON.stringify(profile)) },
+  };
+});
+
+export default Page;
